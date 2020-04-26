20332a61accbd0dc587b4e46c6184a795cee0300
shukhrat2001@gmail.com <shukhrat2001@gmail.com>
2020-04-26 17:29:51 -0400

    New Svelte project

---
+++ src/App.svelte
@@ -0,0 +1,29 @@
+<script>
+	export let name;
+</script>
+
+<main>
+	<h1>Greetings {name}!</h1>
+	<p>Visit the <a href="https://sneg.herokuapp.com ">SNEG SOFTWARE </a> to learn how to build Fast Website apps.</p>
+</main>
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
